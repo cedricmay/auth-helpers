380e86c68c2d44835ce53b9e5d8f0d7682a48df5
Cedric May <hallo@cedric-may.de>
2023-10-12 19:17:33 +0200

    sameSite: none

--- packages/shared/src/utils/constants.ts
+++ packages/shared/src/utils/constants.ts
@@ -2,5 +2,6 @@ import { DefaultCookieOptions } from '../types';
 
 export const DEFAULT_COOKIE_OPTIONS: DefaultCookieOptions = {
 	path: '/',
-	maxAge: 60 * 60 * 24 * 365 * 1000
+	maxAge: 60 * 60 * 24 * 365 * 1000,
+	sameSite: 'none'
 };
