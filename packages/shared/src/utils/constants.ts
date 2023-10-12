29aacdfb7edb80a9777f34881b39cc4f95a9ce83
Cedric May <hallo@cedric-may.de>
2023-10-12 19:37:42 +0200

    Updated sameSite to add secure

--- packages/shared/src/utils/constants.ts
+++ packages/shared/src/utils/constants.ts
@@ -3,5 +3,6 @@ import { DefaultCookieOptions } from '../types';
 export const DEFAULT_COOKIE_OPTIONS: DefaultCookieOptions = {
 	path: '/',
 	maxAge: 60 * 60 * 24 * 365 * 1000,
-	sameSite: 'none'
+	sameSite: 'none',
+	secure: true
 };
